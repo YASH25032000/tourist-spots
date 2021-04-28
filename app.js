853987e28964f5c7f07ca8b7930234f8764714a1
YASH25032000 <yashasvisinghdeo945@gmail.com>
2021-04-28 22:46:45 +0530

    image

--- app.js
+++ app.js
@@ -114,7 +114,7 @@ app.use(
                 "'self'",
                 "blob:",
                 "data:",
-                "https://res.cloudinary.com/douqbebwk/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
+                "https://res.cloudinary.com/process.env.CLOUDINARY_CLOUD_NAME/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
                 "https://images.unsplash.com",
             ],
             fontSrc: ["'self'", ...fontSrcUrls],
